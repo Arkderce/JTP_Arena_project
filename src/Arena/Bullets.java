e2be3e32c78db425c91758e9e664354ec19556ba
Arkderce <arkderce@gmail.com>
2018-06-10 18:28:48 +0200

    expand arena

--- src/Arena/Bullets.java
+++ src/Arena/Bullets.java
@@ -11,7 +11,12 @@ public class Bullets {
     public int bulletNumber = 1;
     public double x = 1200;
     public double y = 800;
+    Arena arena;
     HashMap<Integer, List<Double>> bullets = new HashMap<Integer, List<Double>>();
+
+    Bullets(Arena _arena){
+        arena = _arena;
+    }
 
     public void createBullet(int ClientId, double posX, double posY, double angle){
         List<Double> temp = new ArrayList<Double>();
@@ -20,8 +25,9 @@ public class Bullets {
         bulletNumber ++;
     }
 
-    public void moveBullets( Targets oTargets ) {
+    public void moveBullets( Targets oTargets, Walls oWalls, double posX, double posY) {
         HashMap<Integer, List<Double>> targetsPos = oTargets.returnTargetsPos();
+        HashMap<Integer, List<Double>> wallsPos = oWalls.returnWalls();
         List<Integer> toDelete = new ArrayList<Integer>();
         for (Integer bullet : bullets.keySet()) {
             List<Double> bulletSpecs = bullets.get(bullet);
@@ -36,7 +42,7 @@ public class Bullets {
             temp.add(angle);
             temp.add(bulletSpecs.get(3));
 
-            if (!interactionOfBullet(temp, bullet, oTargets, targetsPos)) {
+            if (!interactionOfBullet(temp, bullet, oTargets, targetsPos, oWalls, wallsPos, posX, posY, arena)) {
                 bullets.put(bullet, temp);
             } else {
                 toDelete.add(bullet);
@@ -49,7 +55,8 @@ public class Bullets {
     }
 
     public boolean interactionOfBullet(List<Double> bulletSpecs, Integer bullet, Targets oTargets,
-                                       HashMap<Integer, List<Double>> targetsPos ){
+                                       HashMap<Integer, List<Double>> targetsPos, Walls oWalls,
+                                       HashMap<Integer, List<Double>> wallsPos, double posX, double posY, Arena arena){
         boolean didInteract = false;
         Double bX = bulletSpecs.get(0);
         Double bY = bulletSpecs.get(1);
@@ -58,21 +65,37 @@ public class Bullets {
             didInteract = true;
         }
 
+        if((bX >= posX && bX <= posX + 40) && (bY >= posY && bY <= posY + 40) && clientB != 1){
+            arena.setHealth(arena.getHealth()-10);
+            didInteract = true;
+        }
+
         List<Integer> toInteract = new ArrayList<Integer>();
         for (Integer targetId : targetsPos.keySet()) {
-            List<Double> targetSpecs = targetsPos.get(targetId);
-            Double pX = targetSpecs.get(0);
-            Double pY = targetSpecs.get(1);
-            if((bX >= pX && bX <= pX + 40) && (bY >= pY && bY <= pY + 40)){
-                //oTargets.targetHit(targetId);
-                toInteract.add(targetId);
-                didInteract = true;
+            if(clientB != 2) {
+                List<Double> targetSpecs = targetsPos.get(targetId);
+                Double pX = targetSpecs.get(0);
+                Double pY = targetSpecs.get(1);
+                if ((bX >= pX && bX <= pX + 40) && (bY >= pY && bY <= pY + 40)) {
+                    toInteract.add(targetId);
+                    didInteract = true;
+                }
             }
         }
 
         for(int i = 0; i < toInteract.size(); i++){
             oTargets.targetHit(toInteract.get(i));
         }
+
+        for (Integer wallId : wallsPos.keySet()) {
+            List<Double> wallSpecs = wallsPos.get(wallId);
+            Double pX = wallSpecs.get(0);
+            Double pY = wallSpecs.get(1);
+            if((bX >= pX && bX <= pX + 40) && (bY >= pY && bY <= pY + 40)){
+                didInteract = true;
+            }
+        }
+
 
         return didInteract;
     }
--- src/Arena/Walls.java
+++ src/Arena/Walls.java
@@ -9,7 +9,72 @@ public class Walls {
     private int wallId = 0;
 
     public void createWalls(double x, double y){
-        wallsPos.put(wallId, Arrays.asList(x/2 - 100, y/2 - 100));
+        addWall(x/2 - 100, y/2 - 100);
+        addWall(x/2 - 60, y/2 - 100);
+        addWall(x/2 - 100, y/2 - 60);
+
+        addWall(x/2 - 100, y/2 + 100);
+        addWall(x/2 - 60, y/2 + 100);
+        addWall(x/2 - 100, y/2 + 60);
+
+        addWall(x/2 + 100, y/2 - 100);
+        addWall(x/2 + 60, y/2 - 100);
+        addWall(x/2 + 100, y/2 - 60);
+
+        addWall(x/2 + 100, y/2 + 100);
+        addWall(x/2 + 60, y/2 + 100);
+        addWall(x/2 + 100, y/2 + 60);
+
+        addWall((x-700)/2 - 100, y-200 - 100);
+        addWall((x-700)/2 - 60, y-200 - 100);
+        addWall((x-700)/2 - 100, y-200 - 60);
+
+        addWall((x-700)/2 - 100, y-200 + 100);
+        addWall((x-700)/2 - 60, y-200 + 100);
+        addWall((x-700)/2 - 100, y-200 + 60);
+
+        addWall((x-700)/2 + 100, y-200 - 100);
+        addWall((x-700)/2 + 60, y-200 - 100);
+        addWall((x-700)/2 + 100, y-200 - 60);
+
+        addWall((x-700)/2 + 100, y-200 + 100);
+        addWall((x-700)/2 + 60, y-200 + 100);
+        addWall((x-700)/2 + 100, y-200 + 60);
+
+        addWall(x-200, y-240);
+        addWall(x-240, y-240);
+        addWall(x-240, y-280);
+        addWall(x-280, y-280);
+
+
+        addWall(x-700, y-700);
+        addWall(x-800, y-700);
+        addWall(x-900, y-700);
+        addWall(x-1000, y-700);
+        addWall(x-1100, y-700);
+
+        addWall(x - 100, y-100);
+        addWall(x - 100, y-140);
+        addWall(x - 100, y-180);
+
+        addWall(x - 100, y-740);
+        addWall(x - 140, y-740);
+        addWall(x - 140, y-700);
+        addWall(x - 140, y-660);
+        addWall(x - 140, y-620);
+        addWall(x - 140, y-580);
+
+        addWall(x - 100, y-700);
+        addWall(x - 180, y-700);
+        addWall(x - 220, y-700);
+
+        addWall(x-200 + 100, y-300 + 100);
+        addWall(x-200 + 60, y-300 + 100);
+        addWall(x-200 + 100, y-300 + 60);
+    }
+
+    public void addWall(double x, double y){
+        wallsPos.put(wallId, Arrays.asList(x, y));
         wallId++;
     }
 
